8e7570b163c461fbb98df963e7772370e1966acb
AndyN <avident@gmail.com>
2017-12-05 00:12:16 -0800

    main push

---
+++ friends.js
@@ -0,0 +1,96 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Star Wars Characters (DATA)
+// =============================================================
+var friends = [
+  {
+    name: "Happy",
+    scores:[]
+  },
+  {
+    name: "Miku",
+    scores:[]
+  },
+  {
+    name: "Bob",
+    scores:[]
+  }
+];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+// Get all characters
+app.get("/all", function(req, res) {
+  res.json(characters);
+});
+
+// Search for Specific Character (or all characters) - provides JSON
+app.get("/api/:characters?", function(req, res) {
+  var chosen = req.params.characters;
+
+  if (chosen) {
+    console.log(chosen);
+
+    for (var i = 0; i < characters.length; i++) {
+      if (chosen === characters[i].routeName) {
+        return res.json(characters[i]);
+      }
+    }
+    return res.json(false);
+  }
+  return res.json(characters);
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/new", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newfriend = req.body;
+
+  console.log(newfriend);
+
+  friends.push(newcharacter);
+
+  res.json(newcharacter);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
+var store = []
+for (var i = 0; i < score.length; i++) {
+    var diff = Math.abs(score[i]-score2[i])
+    store.push(diff)
+
+    function getSum(total, num) {
+        return total + num;
+    }
+    getSum(store)
+
+}
